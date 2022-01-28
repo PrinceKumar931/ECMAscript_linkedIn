75c5bc4acc92a4de54ae6acccbe6af50f582757f
Prince Kumar <princekumar931083@gmail.com>
2022-01-28 12:49:56 +0530

    destructuring arrys

---
+++ destructuring arrays/javaScript/index.js
@@ -0,0 +1,10 @@
+let [first,,,,sixth]=[
+    "Attack On Titan",
+    "Hunter Hunter",
+    "Demon Slayer",
+    "Jujutsu Jujutsu Kaisen",
+    "Black Clover",
+    "Death Note"
+]
+console.log(first);
+console.log(sixth);
